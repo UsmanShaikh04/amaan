dbce1c000fddc6891350647e81871542d0223bc5
UsmanShaikh04 <104716306+UsmanShaikh04@users.noreply.github.com>
2023-04-18 16:26:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: ["./src/**/*.{html,vue,js}"],
+  theme: {  
+    extend: {},
+    // width: {
+    //   '101': '580px',
+    //   '102': '480px',
+    // },
+    // height:{
+    //   '401': '40vh'
+    // },
+    // margin:{
+    //   '10':'10vh'
+    // }
+   
+  },
+  plugins: [],
+}
+
